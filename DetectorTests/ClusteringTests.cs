cb06996f947786a737fe85b83b0963d74afdfb49
ytakashina <takashina2051@gmail.com>
2017-01-08 21:07:16 +0900

    Add testcase

---
+++ DetectorTests/ClusteringTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Detector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detector.Tests
+{
+    [TestClass()]
+    public class ClusteringTests
+    {
+        [TestMethod()]
+        public void DistanceFromSingleToClusterTest()
+        {
+        }
+
+        [TestMethod()]
+        public void DistanceFromClusterToClusterTest()
+        {
+        }
+
+        [TestMethod()]
+        public void SingleLinkageTest()
+        {
+        }
+    }
+}
