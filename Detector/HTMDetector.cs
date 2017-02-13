51c1cf07b3aa932203a5f6d0d8f302dd8cea2a5e
ytakashina <takashina2051@gmail.com>
2017-02-13 15:57:38 +0900

    卒論を書くためにやむなく入れた変更を分離。

--- Detector/HTMDetector.cs
+++ Detector/HTMDetector.cs
@@ -15,6 +15,11 @@ namespace Detector
         private int[] _predictedSeries;
         private double[] _errorSeries;
         private double[] _errorSeries2;
+        private double[] _errorSeries3;
+        private double[] _errorSeries4;
+        public double CompressRatioHTM;
+        public double CompressRatioFreq;
+        public double CompressRatioMM;
         // 遷移確率行列、距離行列
         // Level 1
         private readonly double[,] _transitions1 = new double[N1, N1];
@@ -22,6 +27,7 @@ namespace Detector
         private readonly double[,] _distances1Mean = new double[N1, N1];
         private readonly double[,] _distances1Min = new double[N1, N1];
         // Level 2
+        private readonly double[,] _transitions2 = new double[N2, N2];
         private double[,] _probabilities2 = new double[N2, N2];
         private readonly double[,] _distances2Mean = new double[N2, N2];
         private readonly double[,] _distances2Min = new double[N2, N2];
@@ -83,11 +89,25 @@ namespace Detector
                 var sum = cluster1Members[k].Count();
                 for (var j = 0; j < N1; j++)
                 {
-                    _membership12[j, k] = cluster1Members[k].Contains(j) ? 1.0/sum : 1e-6;
+                    // sum で割るのは間違い？
+//                    _membership12[j, k] = cluster1Members[k].Contains(j) ? 1.0/sum : 1e-4;
+                    _membership12[j, k] = cluster1Members[k].Contains(j) ? 1.0 : 1e-4;
                 }
             }
             // Level 2
-            _probabilities2 = _membership12.PseudoInverse().Mul(_probabilities1).Mul(_membership12);
+            for (var i = 0; i < _series.Length - 2; i++)
+            {
+                // cluster1membersを使う
+                int from2 = -1, to2 = -1;
+                for (var j = 0; j < N2; j++)
+                {
+                    if (1 - _membership12[_series[i], j] < 1e-6) from2 = j;
+                    if (1 - _membership12[_series[i + 1], j] < 1e-6) to2 = j;
+                }
+                _transitions2[from2, to2] += 1;
+            }
+            _probabilities2 = _transitions2.NormalizeToRaw();
+//            _probabilities2 = _membership12.PseudoInverse().Mul(_probabilities1).Mul(_membership12);
             for (var j = 0; j < N2; j++)
             {
                 for (var k = 0; k < N2; k++)
@@ -104,7 +124,8 @@ namespace Detector
                 var sum = cluster2Members[k].Count();
                 for (var j = 0; j < N2; j++)
                 {
-                    _membership23[j, k] = cluster2Members[k].Contains(j) ? 1.0/sum : 0;
+                    // sum で割るのは間違い？
+                    _membership23[j, k] = cluster2Members[k].Contains(j) ? 1.0/sum : 1e-4;
                 }
             }
             // Level 3
@@ -124,12 +145,17 @@ namespace Detector
             _predictedSeries = new int[_series.Length];
             _errorSeries = new double[_series.Length];
             _errorSeries2 = new double[_series.Length];
+            _errorSeries3 = new double[_series.Length];
+            _errorSeries4 = new double[_series.Length];
             // 事前分布
+            var memorylessPrior1 = Enumerable.Range(0, N1).Select(i => 1.0/N1).ToArray();
             var prior1 = Enumerable.Range(0, N1).Select(i => (double) _series.Take(_series.Length/2).Count(v => v == i)/_series.Length*2).ToArray();
+//            var prior1 = Enumerable.Range(0, N1).Select(i => (double) _series.Count(v => v == i)/_series.Length).ToArray();
+            prior1 = prior1.Select(v => v > 0 ? v : 1e-4).ToArray();
             var prior2 = _membership12.T().Mul(prior1);
             var state2 = prior2.Select(x => x).ToArray();
             // 予測
-            for (var i = 0; i < _series.Length - 1; i++)
+            for (var i = _series.Length/2; i < _series.Length - 1; i++)
             {
                 // 状態
                 var state1 = Enumerable.Range(0, N1).Select(j => j == _series[i] ? 1.0 : 0.0).ToArray();
@@ -139,12 +165,24 @@ namespace Detector
                 var sum = state2.Sum();
                 for (var j = 0; j < N2; j++) state2[j] = sum < 1e-300 ? prior2[j] : state2[j]/sum;
                 var prediction = _membership12.Mul(_probabilities2.Mul(state2));
+                _predictedSeries[i + 1] = prediction.ToList().IndexOf(prediction.Max());
                 var error = -Math.Log(prediction[_series[i + 1]], 2);
+                if (double.IsNaN(error))
+                {
+                    Console.WriteLine(i);
+                }
                 _errorSeries[i + 1] = double.IsPositiveInfinity(error) ? 100 : error;
                 var error2 = -Math.Log(prior1[_series[i + 1]], 2);
                 _errorSeries2[i + 1] = double.IsPositiveInfinity(error2) ? 100 : error2;
-                _predictedSeries[i + 1] = prediction.ToList().IndexOf(prediction.Max());
+                _errorSeries3[i + 1] = -Math.Log(memorylessPrior1[_series[i + 1]], 2);
+                var error4 = -Math.Log(_probabilities1.Mul(state1)[_series[i + 1]], 2);
+                _errorSeries4[i + 1] = double.IsPositiveInfinity(error4) ? 100 : error4;
             }
+            var averageInformationMemolyless = _errorSeries3.Average();
+            CompressRatioHTM = _errorSeries.Average()/averageInformationMemolyless;
+            CompressRatioFreq = _errorSeries2.Average()/averageInformationMemolyless;
+            CompressRatioMM = _errorSeries4.Average()/averageInformationMemolyless;
+            Console.WriteLine("HTM: " + CompressRatioHTM + ", Freq: " + CompressRatioFreq + ", MM: " + CompressRatioMM);
             //_mutualInformations.ForEach(v => Console.Write(v.ToString("F4") + "\n"));
             //var g1 = _membership12.Mul(_membership23.Mul(new double[] {1, 0, 0, 0}));
             //var g2 = _membership12.Mul(_membership23.Mul(new double[] {0, 1, 0, 0}));
@@ -179,16 +217,18 @@ namespace Detector
             SaveMatrixImage(_distances1Mean, path + "layer1_distances_mean", threshold: double.MaxValue, bgWhite: false);
             SaveMatrixImage(_distances1Min, path + "layer1_distances_min", threshold: double.MaxValue, bgWhite: false);
             SaveMatrixImage(_membership12, path + "layer12_membership");
-            SaveMatrixImage(_probabilities2, path + "layer2_probabilities");
-            SaveMatrixImage(_distances2Mean, path + "layer2_distances_mean", threshold: double.MaxValue, bgWhite: false);
-            SaveMatrixImage(_distances2Min, path + "layer2_distances_min", threshold: double.MaxValue, bgWhite: false);
-            SaveMatrixImage(_membership23, path + "layer23_membership");
-            SaveMatrixImage(_probabilities3, path + "layer3_probabilities");
-            SaveMatrixImage(_distances3Mean, path + "layer3_distances_mean", threshold: double.MaxValue, bgWhite: false);
-            SaveMatrixImage(_distances3Min, path + "layer3_distances_min", threshold: double.MaxValue, bgWhite: false);
+//            SaveMatrixImage(_probabilities2, path + "layer2_probabilities");
+//            SaveMatrixImage(_distances2Mean, path + "layer2_distances_mean", threshold: double.MaxValue, bgWhite: false);
+//            SaveMatrixImage(_distances2Min, path + "layer2_distances_min", threshold: double.MaxValue, bgWhite: false);
+//            SaveMatrixImage(_membership23, path + "layer23_membership");
+//            SaveMatrixImage(_probabilities3, path + "layer3_probabilities");
+//            SaveMatrixImage(_distances3Mean, path + "layer3_distances_mean", threshold: double.MaxValue, bgWhite: false);
+//            SaveMatrixImage(_distances3Min, path + "layer3_distances_min", threshold: double.MaxValue, bgWhite: false);
             ChartExtensions.CreateChart(_series.Select(i => _samplePoints[i]).ToArray()).SaveImage(path + "original");
             ChartExtensions.CreateChart(_errorSeries).SaveImage(path + "error");
             ChartExtensions.CreateChart(_errorSeries2).SaveImage(path + "error2");
+            ChartExtensions.CreateChart(_errorSeries3).SaveImage(path + "error3");
+            ChartExtensions.CreateChart(_errorSeries4).SaveImage(path + "error4");
         }
     }
 }
--- DetectorTests/HTMDetectorTests.cs
+++ DetectorTests/HTMDetectorTests.cs
@@ -14,7 +14,7 @@ namespace Detector.Tests
         public void DetectTest()
         {
             var files = new DirectoryInfo(@"..\data").GetDirectories().SelectMany(dir => dir.GetFiles()).Select(f => f.FullName);
-            Parallel.ForEach(files, file =>
+            Func<string, double> func = file =>
             {
                 var detector = new HTMDetector();
                 using (var dr = new DataReader(file))
@@ -25,11 +25,27 @@ namespace Detector.Tests
                 detector.Learn();
                 detector.Predict();
                 detector.SaveResultImages("results\\" + file.Split('\\').Last().Split('.').First());
-            });
-            //var path = @"..\data\realAWSCloudwatch\grok_asg_anomaly.csv";
-            //var path = @"..\data\artificialWithAnomaly\art_daily_jumpsdown.csv";
-            //var path = @"..\data\artificialWithAnomaly\art_load_balancer_spikes.csv";
-            //var path = @"..\data\realAWSCloudwatch\ec2_cpu_utilization_5f5533.csv";
+                return detector.CompressRatioHTM;
+            };
+            var query = from file in files.AsParallel() select func(file);
+            var x = query.Average();
+            Console.WriteLine(x);
+//            Parallel.ForEach(files, file =>
+//            {
+//                var detector = new HTMDetector();
+//                using (var dr = new DataReader(file))
+//                {
+//                    var data = dr.GetLines();
+//                    detector.Initialize(data);
+//                }
+//                detector.Learn();
+//                detector.Predict();
+//                detector.SaveResultImages("results\\" + file.Split('\\').Last().Split('.').First());
+//            });
+//            var path = @"..\data\realAWSCloudwatch\grok_asg_anomaly.csv";
+//            var path = @"..\data\artificialWithAnomaly\art_daily_jumpsdown.csv";
+//            var path = @"..\data\artificialWithAnomaly\art_load_balancer_spikes.csv";
+//            var path = @"..\data\realAWSCloudwatch\ec2_cpu_utilization_5f5533.csv";
 //            var detector = new HTMDetector();
 //            using (var dr = new DataReader(path))
 //            {
