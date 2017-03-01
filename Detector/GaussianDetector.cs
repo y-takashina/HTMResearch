baec2d2f640c1223fdb7a4b414248b30b954e671
ytakashina <takashina2051@gmail.com>
2017-03-01 12:47:43 +0900

    閾値法の追加

---
+++ Detector/GaussianDetector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PipExtensions;
+using static PipExtensions.PipExtensions;
+
+namespace Detector
+{
+    public static class GaussianDetector
+    {
+        public static bool[] AnomalySeries(IEnumerable<double> rawData)
+        {
+            var array = rawData.ToArray();
+            var dataNotNaN = array.Where(v => !double.IsNaN(v)).ToArray();
+            var mean = dataNotNaN.Average();
+            var stddev = dataNotNaN.StandardDeviation();
+            return array.Select(v => !double.IsNaN(v) && QFunction(v, mean, stddev) < 1e-10).ToArray();
+        }
+    }
+}
---
+++ DetectorTests/GaussianDetectorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Detector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detector.Tests
+{
+    [TestClass()]
+    public class GaussianDetectorTests
+    {
+        [TestMethod()]
+        public void DetectTest()
+        {
+            var rawSeries = new List<List<double>>();
+            using (var sr = new StreamReader(@"..\data\water-treatment.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',').Skip(1).ToArray();
+                    if (!rawSeries.Any()) foreach (var _ in line) rawSeries.Add(new List<double>());
+                    for (var i = 0; i < line.Length; i++)
+                    {
+                        double value;
+                        rawSeries[i].Add(double.TryParse(line[i], out value) ? value : double.NaN);
+                    }
+                }
+            }
+
+            var anomalySeries = new List<bool>();
+            var anomaly = rawSeries.Select(GaussianDetector.AnomalySeries).ToArray();
+            for (var i = 0; i < rawSeries.First().Count; i++)
+            {
+                var isAnomaly = anomaly.Aggregate(false, (current, series) => current || series[i]);
+                anomalySeries.Add(isAnomaly);
+            }
+            anomalySeries.ForEach(Console.WriteLine);
+        }
+    }
+}
