0a95ff868233daea5f0511c441b1de0a0ad118ec
ytakashina <takashina2051@gmail.com>
2017-03-14 14:28:18 +0900

    さすがにコードを書くのがきつくなってきたのでリファクタリング

---
+++ Detector/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detector
+{
+    public abstract class Node<T>
+    {
+        public IEnumerable<T> SpatialPooler { get; set; }
+        public IEnumerable<T> TemporalPooler { get; set; }
+        public IEnumerable<Node<T>> ChildNodes { get; set; }
+        public int[,] Membership { get; set; }
+
+        public abstract void Forward();
+        public abstract void Backward();
+        public abstract void Learn();
+        public abstract void Predict();
+    }
+
+    public class LeafNode<T> : Node<T>
+    {
+        public LeafNode(IEnumerable<T> inputStream)
+        {
+            ChildNodes = new Node<T>[] {};
+            SpatialPooler = inputStream.Distinct();
+        }
+
+        public override void Forward() {}
+        public override void Backward() {}
+        public override void Learn() {}
+        public override void Predict() {}
+    }
+}
