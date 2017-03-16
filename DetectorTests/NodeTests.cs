811ac921613831402db5f4e059cd97facd3a69ce
ytakashina <takashina2051@gmail.com>
2017-03-16 15:53:56 +0900

    Forwardのテストを追加

--- DetectorTests/NodeTests.cs
+++ DetectorTests/NodeTests.cs
@@ -21,7 +21,26 @@ namespace Detector.Tests
         }
 
         [TestMethod()]
-        public void ForwardTest() {}
+        public void ForwardTest()
+        {
+            var inputs = new[]
+            {
+                new[] {1, 0, 0, 0, 0},
+                new[] {0, 1, 0, 0, 0},
+                new[] {0, 0, 1, 0, 0},
+                new[] {0, 0, 0, 1, 0},
+                new[] {0, 0, 0, 0, 1},
+            };
+            var answers = new[,] {{0, 1}, {0, 1}, {0, 1}, {1, 0}, {1, 0}};
+            for (var i = 0; i < 5; i++)
+            {
+                var output = _node.Forward(inputs[i]);
+                for (var j = 0; j < 2; j++)
+                {
+                    Assert.AreEqual(answers[i, j], output[j]);
+                }
+            }
+        }
 
         [TestMethod()]
         public void ForwardTest1() {}
@@ -35,7 +54,7 @@ namespace Detector.Tests
         [TestMethod()]
         public void LearnTest()
         {
-            var ans = new[,] {{1, 0}, {1, 0}, {1, 0}, {0, 1}, {0, 1}};
+            var ans = new[,] {{0, 1}, {0, 1}, {0, 1}, {1, 0}, {1, 0}};
             for (var i = 0; i < 5; i++)
             {
                 for (var j = 0; j < 2; j++)
